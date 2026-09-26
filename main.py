7485ab84bc470c06a49d3c4a3d67b54106ed76d5
d58j86vqtc-svg <d58j86vqtc@privaterelay.appleid.com>
2026-09-26 12:49:38 +0300

    v1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+
 import os
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
@@ -17,17 +18,56 @@ supabase: Client = create_client(DB_LINK, DB_KEY)
 app = Flask(__name__)
 
 
-@app.get("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.get("/users")
-def get_users():
-    response = supabase.table("users").select("*").execute()
+@app.route("/messages", methods=["GET"])
+def get_messages():
+    response = (
+        supabase
+        .table("messages")
+        .select("*")
+        .order("created_at", desc=False)
+        .execute()
+    )
 
     return jsonify(response.data)
 
 
+@app.route("/messages", methods=["POST"])
+def send_message():
+    data = request.get_json()
+
+    username = data.get("username", "").strip()
+    message = data.get("message", "").strip()
+
+    if not username:
+        return jsonify({"error": "Введи ім'я"}), 400
+
+    if not message:
+        return jsonify({"error": "Введи повідомлення"}), 400
+
+    if len(username) > 30:
+        return jsonify({"error": "Ім'я занадто довге"}), 400
+
+    if len(message) > 1000:
+        return jsonify({"error": "Повідомлення занадто довге"}), 400
+
+    response = (
+        supabase
+        .table("messages")
+        .insert({
+            "username": username,
+            "message": message
+        })
+        .execute()
+    )
+
+    return jsonify(response.data[0]), 201
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
